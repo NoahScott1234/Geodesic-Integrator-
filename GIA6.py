9bdfa7cff51fe8f3f851fadbadcb641c8f21abec
NoahScott1234 <noahscott1929@gmail.com>
2026-08-15 22:23:40 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- GIA6.py
+++ GIA6.py
@@ -146,7 +146,7 @@ with st.sidebar.expander('Settings'):
     else:
         spin_parameter = 0
     mass = st.number_input('Object Mass', min_value=0.0, max_value = 10000, value = 1)
-    sim_run_time = st.number_input('Simulation Run Time (Proper Time)', min_value = 0.0, max_value = 1000000 value = 100)
+    sim_run_time = st.number_input('Simulation Run Time (Proper Time)', min_value = 0.0, max_value = 1000000, value = 100)
 
 columns = st.sidebar.columns(3)
 for mu in range (3):
